a4618ebf3ae85c9b1a86d824435693986f729c3e
jeremie <jeremie@hemisphere-project.com>
2021-07-14 10:53:47 +0200

    S6 ok

--- WebApps/S6-MULTIPLAYER/scripts.js
+++ WebApps/S6-MULTIPLAYER/scripts.js
@@ -1,3 +1,5 @@
+//////////////// NAVIGATION ////////////////
+
 $('.page').hide()
 $('#page_home').show()
 
@@ -19,35 +21,151 @@ $('#goVideo').click(function(){
   })
 })
 
+$('#btn_close_gallerylist').click(function(){
+  $('#page_gallery_list').hide()
+  $('#page_list').fadeIn(300)
+})
+
+$('#btn_close_gallery').click(function(){
+  $('#page_gallery').hide()
+  $('#page_gallery_list').fadeIn(300)
+})
+
+
+
+
 
 
 //////////////// VIDEO PLAYER ////////////////
 
+var scrollBarUpdate;
 
 function playFilm(){
-
   $("#videoplayer")[0].play();
+  //SCROLLBAR INTERVAL
+  scrollBarUpdate = setInterval(function(){
+    var currentTime = $('#videoplayer')[0].currentTime
+    var videoDuration = $("#videoplayer")[0].duration
+    var percent = currentTime*100/videoDuration
+    $('#scrollbar').css('width', percent+'%')
+  }, 20)
 }
 
+//SCROLLBAR AUTO
+// $('#videoplayer').on('timeupdate', function(e){
+//   var currentTime = e.target.currentTime
+//   var videoDuration = $("#videoplayer")[0].duration
+//   var percent = currentTime*100/videoDuration
+//   $('#scrollbar').css('width', percent+'%')
+// })
 
-//SCROLLBAR
-///////////////////////////////////////////////////////////
+//SCROLLBAR CLICK
+$('#scrollbarContainer').on('click',function(e){
+  var offset = $(this).offset()
+  var relX = e.pageX - offset.left
+  var percent = ( relX / $(this).width() )*100
+  var videoDuration = $("#videoplayer")[0].duration
+  var time2Seek=percent*videoDuration/100
+  $('#videoplayer')[0].currentTime = time2Seek
+  $('#scrollbar').css('width', percent+'%')
+});
 
-  $('#scrollbarContainer').on('click',function(e){
-    var offset = $(this).offset();
-    var relX = e.pageX - offset.left;
-    var percent = ( relX / $(this).width() )*100;
-    var videoDuration = $("#videoplayer")[0].duration;
-    var time2Seek=percent*videoDuration/100;
-    $('#videoplayer')[0].currentTime = time2Seek;
-    // $('#scrollbar').css('margin-left', percent+'%');
-    $('#scrollbar').css('width', percent+'%');
-  });
 
-  $('#videoplayer').on('timeupdate', function(e){
-    var currentTime = e.target.currentTime;
-    var videoDuration = $("#videoplayer")[0].duration;
-    var percent = currentTime*100/videoDuration;
-    // $('#scrollbar').css('margin-left', percent+'%');
-    $('#scrollbar').css('width', percent+'%');
-  });
+// END || CLOSE
+$('#videoplayer').on('ended',function(){
+  $('#page_video').hide()
+  $('#page_list').fadeIn(300)
+  clearInterval(scrollBarUpdate)
+  $('#scrollbar').css('width', '0%')
+  $("#videoplayer")[0].currentTime = 0
+});
+$('#btn_close_video').click(function(){
+  $('#page_video').hide()
+  $('#page_list').fadeIn(300)
+  clearInterval(scrollBarUpdate)
+  $('#scrollbar').css('width', '0%')
+  $("#videoplayer")[0].currentTime = 0
+})
+
+//AUTO HIDE CONTROLS
+var idleTime = 3000;
+var autoHideControls = function() {
+  var timer;
+
+  window.onload = timerReset;
+  document.onkeypress = timerReset;
+  document.onmousemove = timerReset;
+  document.onmousedown = timerReset;
+  document.ontouchstart = timerReset;
+  document.onclick = timerReset;
+  document.onscroll = timerReset;
+  document.onkeypress = timerReset;
+
+  function timerElapsed() {
+    hideControls();
+  };
+  function timerReset() {
+    showControls();
+    clearTimeout(timer);
+    timer = setTimeout(timerElapsed, idleTime);
+  }
+  function hideControls(){
+    $('#scrollbarContainer, #btn_close_video').fadeOut(300);
+  }
+  function showControls(){
+    $('#scrollbarContainer, #btn_close_video').fadeIn(300);
+  }
+};
+autoHideControls();
+
+
+
+
+//////////////// GALLERY ////////////////
+var indexDisplay = 1
+var galleryLength = $('.gallery_item').length
+
+// NEXT
+$('#btn_next').click(function(){
+  indexDisplay ++
+  if (indexDisplay>galleryLength){indexDisplay=1}
+  gotoIndex(indexDisplay)
+})
+// HAMMERJS
+var pantype;
+var mc = new Hammer($("#page_gallery")[0]);
+  mc.on("panleft panright", function(ev) { pantype = ev.type; });
+  mc.on("panend", function(ev) {
+  if(pantype == 'panright'){
+    indexDisplay --
+    if (indexDisplay<0){indexDisplay=galleryLength}
+  }else if(pantype == 'panleft'){
+    indexDisplay ++
+    if (indexDisplay>galleryLength){indexDisplay=1}
+  }
+  console.log(indexDisplay);
+  gotoIndex(indexDisplay)
+});
+
+
+function gotoIndex(index){
+  $('.gallery_item.active').fadeOut(200,function(){
+    $(this).removeClass('active');
+    $('.gallery_item:nth-child('+index+')').addClass('active');
+    $('.gallery_item:nth-child('+index+')').fadeIn(200);
+  })
+}
+
+//////////////// INIT GALLERY ////////////////
+$('#page_gallery_list .list_item').click(function(){
+
+  $('#page_gallery_list').hide()
+  $('#page_gallery').fadeIn(300)
+
+  indexDisplay = $(this).attr('galleryIndex');
+  $('.gallery_item').hide();
+  $('.gallery_item:nth-child('+indexDisplay+')').addClass('active');
+  $('.gallery_item:nth-child('+indexDisplay+')').fadeIn(200);
+
+
+})
--- WebApps/assets/common.js
+++ WebApps/assets/common.js
@@ -1,6 +1,6 @@
 // BLOCK KEYS
 document.onkeydown = function (e) {
-  console.log(e.keyCode);
+  // console.log(e.keyCode);
   if (e.keyCode >= 112 && e.keyCode <= 123) return false;	// Block F1->F12
   if (e.keyCode == 18) return false;				// Block Alt
 };
