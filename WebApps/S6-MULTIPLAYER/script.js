88eec5af3fc4b086f4767910a18099a66d64f776
jeremie <jeremie@hemisphere-project.com>
2021-07-14 18:26:41 +0200

    Update script.js

--- WebApps/S6-MULTIPLAYER/script.js
+++ WebApps/S6-MULTIPLAYER/script.js
@@ -161,9 +161,9 @@ $('#page_gallery_list .list_item').click(function(){
 //   console.log('pinching');
 // });
 
-// var hammertime = new Hammer($('body')[0]);
-// hammertime.get('pinch').set({ enable: true });
-// hammertime.on("pinch", function(e) { console.log("pinching 2") });
+var hammertime = new Hammer($('body')[0]);
+hammertime.get('pinch').set({ enable: true });
+hammertime.on("pinch", function(e) { console.log("pinching body") });
 
 
 function attachPinch(indexDisplay){
